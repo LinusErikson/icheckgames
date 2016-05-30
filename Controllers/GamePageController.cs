f543e6c315bff373cd8614ac067d07662a66ed82
Linus Erikson <Linuserikson@gmail.com>
2016-05-30 14:35:10 +0200

    first commit

---
+++ Controllers/GamePageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SteamApiTest.Controllers
+{
+    public class GamePageController : Controller
+    {
+        // GET: GamePage
+        public ActionResult Game(string searchString)
+        {
+            ViewBag.searchString = searchString;
+            return View();
+        }
+    }
+}
---
+++ scripts/search.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        crossDomain: true,
+        jsonp: 'json_callback',
+        url: 'http://www.giantbomb.com/api/search/?format=jsonp',
+        data: { api_key: "d46581bb69a487551688b5e5fdc7a93fa8630096", query: $("#searchField").val() },
+        complete: function () {
+            console.log('done');
+        },
+        success: function (data) {
+            debugger;
+            data.results.forEach(function (game) {
+                $('#gameResult').append('<li>' + game.name + '</li>');
+                $('#picResult').append('<li><img src=' + game.image.medium_url + '></li>');
+            })
+        }
+    })
+});
