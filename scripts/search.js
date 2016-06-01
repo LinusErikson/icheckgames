9ec0907c8e613a7f2ad6b8c4a46999a35503e4e1
Linus Erikson <Linuserikson@gmail.com>
2016-06-01 11:22:58 +0200

    fixed searchToProfile Logicz

--- scripts/search.js
+++ scripts/search.js
@@ -1,8 +1,4 @@
-﻿
-var ids = [];
-var id = ['Row' + 1];
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
         $.ajax({
             type: 'GET',
             dataType: 'jsonp',
@@ -15,19 +11,14 @@ $(document).ready(function () {
             },
             success: function (data) {
                 data.results.forEach(function (game) {
+                   
+                    var url = "/gamepage/gameprofile/" + game.id;
+                    $('#searchResults').append('<tr data-href='+url+' class=' + 'eztr><td class=' + 'eztd>' + game.name + '<img class=' + 'ezimg' + ' src=' + game.image.icon_url + '>' + '</td></tr>');
 
-                    $('#searchResults').append('<tr id='+id+' class=' + 'eztr><td class=' + 'eztd>' + game.name + '<img class=' + 'ezimg' + ' src=' + game.image.icon_url + '>' + '</td></tr>');
-                    $('tr').each(function (index) {
-                        id.pop();
-                        id.push('Row' + (index + 2));
-                    });
-                    ids.push({ "id": game.id })
-
-                    $('#'+id).click(function () {
-                        window.location.href = '/GamePage/GameProfile/3030-';
+                    $('.eztr').click(function () {
+                        window.location = $(this).data("href");
                     })
 
-                   
                 })
               
             }
